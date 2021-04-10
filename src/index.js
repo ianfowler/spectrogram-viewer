202b6bd3066835c096fecf3ca30221bf2704afa6
Ian Fowler <ian@spidr.design>
2021-04-10 01:58:50 -0500

    comment index.js

--- src/index.js
+++ src/index.js
@@ -1,23 +1,42 @@
-
-  
 /**
  * Name: Ian Fowler
  * CS 101 Spring 2021
  * Date: April 10th, 2021
  * This is the index.js page for Spectrogram Viewer.
- * It implements the functionality for adding 
+ * It implements the functionality for adding
  * spectrograms to the page using the library SpectrogramJS.
  * The library is located here: https://github.com/vlandham/spectrogramJS
  */
 
+// Keep a tally of all sections added to the DOM to maintain unique IDs.
 let count = 0;
 
+/**
+ * deleteCell:
+ *  Remove a spectrogram and its corresponding section from the DOM
+ *
+ *  Arguments:
+ *    id - the id of the section to remove
+ *
+ *  Returns: None.
+ */
 deleteCell = (id) => {
   var myobj = document.getElementById(id);
   console.log(id);
   if (myobj) myobj.remove();
 };
 
+/**
+ * addSpectrogram:
+ *  Inject a sepctrogram into the surrounding html from addFile
+ *
+ *  Arguments:
+ *    target - the response recieved by the input element which contained file
+ *    file - the audio file to add
+ *    id - the id of the div in which to inject a spectrogram
+ *
+ *  Returns: None.
+ */
 addSpectrogram = (target, file, id) => {
   var reader = new FileReader();
   if (target.files && file) {
@@ -44,6 +63,16 @@ addSpectrogram = (target, file, id) => {
   }
 };
 
+/**
+ * addFile:
+ *  Injects html surrounding a new spectrogram
+ *
+ *  Arguments:
+ *    file - the audio file to add
+ *    target - the response recieved by the input element which contained file
+ *
+ *  Returns: None.
+ */
 addFile = async (file, target) => {
   let sectionID = `sec${count}`;
   let deleteButtonID = `id${count}`;
@@ -70,17 +99,15 @@ addFile = async (file, target) => {
   count += 1;
 };
 
-loadFiles = (target) => {
-  let fileList = target.files;
-  for (var i = 0; i < fileList.length; i++) {
-    let file = fileList.item(i);
-    addFile(file, target);
-  }
-};
-
+// Listen for files to be uploaded, then send to addFile
 window.onload = () => {
   fileSelector = document.getElementById("file-selector");
   fileSelector.addEventListener("change", (event) => {
-    loadFiles(event.target);
+    let target = event.target;
+    let fileList = target.files;
+    for (var i = 0; i < fileList.length; i++) {
+      let file = fileList.item(i);
+      addFile(file, target);
+    }
   });
 };
